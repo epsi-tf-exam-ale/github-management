0d22bc65af679b7d7464fdeaf2c761740c00740b
Salombo <44775059+Salombo@users.noreply.github.com>
2021-05-05 16:10:09 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,3 @@
-terraform {
-  backend "remote" {
-    hostname = "app.terraform.io"
-    organization = "EPSI-ADRIEN"
-
-    workspaces {
-      name = "github-management"
-    }
-  }
-}
-
-
 module "Team_Creation" {
   source = "./module/"
   
